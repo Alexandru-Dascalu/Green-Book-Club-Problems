d670854ed5e9d3fc168efaf34547dacfd15f71a4
Alexandru Dascalu <34415597+Alexandru-Dascalu@users.noreply.github.com>
2018-04-21 18:51:27 +0100

    Add files via upload

---
+++ SuperNumber.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class SuperNumber 
+{
+	public static void main (String[] args)
+	{
+		Scanner kb=new Scanner (System.in);
+		
+		int n=kb.nextInt();
+		int k=kb.nextInt();
+		int superDigit=0,copy, nLength=0;
+		
+		//so that we do not lose the value of n when finding its length
+		copy=n;
+		
+		//find out the number of digits n has
+		while (copy>0)
+		{
+			copy=copy/10;
+			nLength++;
+		}
+		
+		//concatenate n three times
+		for (;k>0;k--)
+		{
+			//multiply it so that we have room for n
+			for(int i=nLength;i>0;i--)
+			{
+				superDigit*=10;
+			}
+			
+			superDigit+=n;
+		}
+		
+		//call out the method
+		System.out.println("The super digit of "+superDigit+" is "+superNumber(superDigit));
+		
+		kb.close();
+		
+	}
+	
+	private static int superNumber(int superDigit)
+	{
+		if(superDigit>9)
+		{
+			int sum=0;
+			
+			//we calculate the sum of its digits
+			while(superDigit>0)
+			{
+				sum+=superDigit%10;
+				superDigit=superDigit/10;
+			}
+			
+			//call out the method recursively
+			superDigit=superNumber(sum);
+			return superDigit;
+		}
+		else 
+		{
+			// if its a single digit then we just return it
+			return superDigit;
+		}
+	}
+}
