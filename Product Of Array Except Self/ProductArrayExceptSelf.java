0b8652908efbee107c1506126847278e9e54497d
Alexandru Dascalu <34415597+Alexandru-Dascalu@users.noreply.github.com>
2018-05-28 15:50:10 +0100

    Update ProductArrayExceptSelf.java
    Added comments and made it so the results are written to an output array and do not overwrite the initial array.

--- Product Of Array Except Self/ProductArrayExceptSelf.java
+++ Product Of Array Except Self/ProductArrayExceptSelf.java
@@ -4,12 +4,15 @@ public class ProductArrayExceptSelf
 {
 	public static void main(String[] args)
 	{
+		//read the size of the array
 		Scanner kb= new Scanner(System.in);
 		System.out.println("Type in how many elements you want in your array:");
 		int nrOfElements=kb.nextInt();
 		
+		//make the array with the right size
 		int[] intArray=new int[nrOfElements];
 		
+		//read the array elements
 		System.out.println("Type in that many numbers to put in the array,"+
 				" each on a new line:");
 		for(int i=0;i<nrOfElements;i++)
@@ -18,35 +21,47 @@ public class ProductArrayExceptSelf
 		}
 		kb.close();
 		
+		//display the array before
 		System.out.println("Array before:");
 		for(int i=0;i<nrOfElements;i++)
 		{
 			System.out.print(intArray[i]+", ");
 		}
 		
-		productExceptSelf(intArray);
+		//calculate output array
+		int[] output=productExceptSelf(intArray);
 		
+		//display the output array
 		System.out.println("\nArray after:");
 		for(int i=0;i<nrOfElements;i++)
 		{
-			System.out.print(intArray[i]+", ");
+			System.out.print(output[i]+", ");
 		}
 	}
 	
-	private static void productExceptSelf(int[] intArray)
+	private static int[] productExceptSelf(int[] intArray)
 	{
+		int[] output=new int [intArray.length];
+		//one is the neutral element for multiplication
 		int productOfElements=1;
 		
+		//we calculate the total product of the elements in the input array
 		for(int i=0;i<intArray.length;i++)
 		{
 			productOfElements*=intArray[i];
 		}
 		
+		/*we calculate the product of the array without that element and without
+		division by raising the current element to the -1 power, thus getting its 
+		inverse number, then multiply the inverse number with the total product to 
+		get the product without the current element*/
 		for(int i=0;i<intArray.length;i++)
 		{
 			double inverseOfNumber=Math.pow(intArray[i], -1);
 			double productExceptSelf=productOfElements*inverseOfNumber;
-			intArray[i]=(int)productExceptSelf;
+			output[i]=(int)productExceptSelf;
 		}
+		
+		return output;
 	}
 }
